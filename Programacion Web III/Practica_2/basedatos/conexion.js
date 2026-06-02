b931728e0e6ee76cc53507ee530e6ff8eff74e3b
Alex-AQ <amarualex83@gmail.com>
2026-06-02 02:10:30 -0400

    Set up MySQL connection in conexion.js

--- Programacion Web III/Practica_2/basedatos/conexion.js
+++ Programacion Web III/Practica_2/basedatos/conexion.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql2');
 
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'practica2web3'
+});
+
+conexion.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Conexion exitosa');
+    }
+});
+
+module.exports = conexion;
