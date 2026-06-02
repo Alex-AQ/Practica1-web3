9bbb2caf7e3965a3d8fbfc410ebf2ab07be5422d
Alex-AQ <amarualex83@gmail.com>
2026-06-02 02:06:19 -0400

    Set up Express server with categorias routes

--- Programacion Web III/Practica_2/app.js
+++ Programacion Web III/Practica_2/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+require('./basedatos/conexion');
 
+const app = express();
+
+app.use(express.json());
+
+const categoriasRoutes = require('./rutas/categorias');
+
+app.use('/categorias', categoriasRoutes);
+
+app.listen(3000, () => {
+    console.log('Servidor ejecutandose en puerto 3000');
+});
